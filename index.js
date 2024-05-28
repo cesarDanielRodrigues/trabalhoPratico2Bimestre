a2f204bd6e037105777f34000268ca6d2e11ad0d
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-05-28 20:14:50 -0300

    Adicionando o DELETE

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const express = require("express");
 const server = express();
+server.use(express.json());
 
 const cursos = ["NodeJS", "JavaScript", "PHP", "React", "VueJS"];
 
+// CREATE
+server.post("/curso", (req, res) => {
+  const { novo_curso } = req.body;
+  cursos.push(novo_curso);
+
+  return res.json(cursos);
+});
+
+// READ
 server.get("/curso", (req, res) => {
   return res.json(cursos);
 });
 
+// READ passando ID
 server.get("/curso/:index", (req, res) => {
   const { index } = req.params;
   return res.json(cursos[index]);
 });
 
-server.post("/curso", (req, res) => {
-  const { novo_curso } = req.body;
-  console.log(novo_curso);
+
+// UPDATE
+server.put("/curso/:index", (req, res) => {
+  const { index } = req.params;
+  const { curso } = req.body;
+
+  cursos[index] = curso;
+
+  return res.json(curso);
 });
 
+//DELETE
+server.delete('/curso/:index', (req, res)=>{
+  const { index } = req.params;
+  cursos.splice(index, 1)
+  return res.json({message: "Curso deletado com sucesso"})
+})
 server.listen(3000);
